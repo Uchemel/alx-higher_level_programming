3d846f1436df1d980d1d9f3ef46a43539301528f
Ogechi Uchechi Edward <edwarduchechi23@gmail.com>
2024-03-29 16:46:04 -0700

    SQL - Introduction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
--- lists all shows contained in the database hbtn_0d_tvshows
--- lists all rows of tables in a database
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Lists all records in the table second_table with a score >= 10 in my MySQL server.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
